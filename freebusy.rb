2d47fe914a51a2c4256e24d56bb02def8c03c079
xandraco <zach@xandra.co>
2016-05-10 07:51:11 +1000

    removed credential callback

--- freebusy.rb
+++ freebusy.rb
@@ -31,17 +31,17 @@ def authorize
     client_id, SCOPE, token_store)
   user_id = 'default'
   credentials = authorizer.get_credentials(user_id)
-  if credentials.nil?
-    url = authorizer.get_authorization_url(
-      base_url: OOB_URI)
-    puts "Open the following URL in the browser and enter the " +
-         "resulting code after authorization"
-    puts url
-    code = gets
-    credentials = authorizer.get_and_store_credentials_from_code(
-      user_id: user_id, code: code, base_url: OOB_URI)
-  end
-  credentials
+  #if credentials.nil?
+  #  url = authorizer.get_authorization_url(
+  #    base_url: OOB_URI)
+  #  puts "Open the following URL in the browser and enter the " +
+  #       "resulting code after authorization"
+  #  puts url
+  #  code = gets
+  #  credentials = authorizer.get_and_store_credentials_from_code(
+  #    user_id: user_id, code: code, base_url: OOB_URI)
+  #end
+  #credentials
 end
 
 # Initialize the API
